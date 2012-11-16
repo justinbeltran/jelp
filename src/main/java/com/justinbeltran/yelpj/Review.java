87ccb1acfcec5d89fb80c47ac56e490db0a5844f
Justin Beltran <justinvbeltran@gmail.com>
2012-11-16 08:58:54 -0800

    First commit

---
+++ src/main/java/com/justinbeltran/yelpj/Review.java
@@ -0,0 +1,11 @@
+package com.justinbeltran.yelpj;
+
+/**
+ * Represents a review specific review from yelp
+ * 
+ * @author justin
+ *
+ */
+public class Review {
+
+}
---
+++ src/main/java/com/justinbeltran/yelpj/ReviewService.java
@@ -0,0 +1,10 @@
+package com.justinbeltran.yelpj;
+
+public class ReviewService {
+
+	public Review getReview(String name, String address) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
